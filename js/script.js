a72ac228dcf22a438bc81f6399e7aa4c4587a70a
jskimweb <jskimweb@gmail.com>
2021-09-08 03:51:34 +0900

    Complete the project (+ responsive)

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
 window.onload = function() {
+
+	// gnb open
 	$('.gnb__menu-1').mouseenter(function() {
 		$('.menu-list-1').show();
+		$(this).css({
+			"color": "#002bd2"
+		})
 	});
 	$('.gnb__menu-1').mouseleave(function() {
 		$('.menu-list-1').hide();
+		$(this).css({
+			"color": "#000"
+		})
 	});
 	$('.menu-list-1').mouseenter(function() {
 		$(this).show();
@@ -20,9 +28,15 @@ window.onload = function() {
 
 	$('.gnb__menu-2').mouseenter(function() {
 		$('.menu-list-2').show();
+		$(this).css({
+			"color": "#002bd2"
+		})
 	});
 	$('.gnb__menu-2').mouseleave(function() {
 		$('.menu-list-2').hide();
+		$(this).css({
+			"color": "#000"
+		})
 	});
 	$('.menu-list-2').mouseenter(function() {
 		$(this).show();
@@ -39,9 +53,15 @@ window.onload = function() {
 	
 	$('.gnb__menu-3').mouseenter(function() {
 		$('.menu-list-3').show();
+		$(this).css({
+			"color": "#002bd2"
+		})
 	});
 	$('.gnb__menu-3').mouseleave(function() {
 		$('.menu-list-3').hide();
+		$(this).css({
+			"color": "#000"
+		})
 	});
 	$('.menu-list-3').mouseenter(function() {
 		$(this).show();
@@ -54,5 +74,24 @@ window.onload = function() {
 		$('.gnb__menu-3').css({
 			"color": "#000"
 		})
+	});
+
+	// visual swiper
+	new Swiper('.sw-visual', {
+		loop: true,
+		speed: 1500,
+		allowTouchMove: false,
+		effect: 'fade',
+		fadeEffect: {
+			crossFade: true,
+		},
+		autoplay: {
+            delay: 5000,
+            disableOnInteraction: false,
+        },
+		navigation: {
+			prevEl: '.sw-visual__prev',
+			nextEl: '.sw-visual__next',
+		},
 	});
 }
